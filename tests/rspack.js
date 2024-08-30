4a7a2d0c8c337338a52401737015ef3e528ae93a
nuintun <nuintun@qq.com>
2024-08-30 17:37:38 +0800

    feat: Update code

--- tests/rspack.js
+++ tests/rspack.js
@@ -6,8 +6,8 @@
 import Koa from 'koa';
 import path from 'path';
 import memfs from 'memfs';
-import rspack from '@rspack/core';
 import compress from 'koa-compress';
+import { rspack } from '@rspack/core';
 import dev from 'rspack-dev-middleware';
 import ReactRefreshPlugin from '@rspack/plugin-react-refresh';
 
@@ -69,6 +69,12 @@ const compiler = rspack({
     parser: {
       'css/auto': {
         namedExports: true
+      }
+    },
+    generator: {
+      'css/auto': {
+        localIdentName: '[local]-[hash:8]',
+        exportsConvention: 'camel-case-only'
       }
     },
     rules: [
